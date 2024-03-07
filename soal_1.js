c461fca0801e4b486d6effcf5e59686514eb8d88
HernandoBun <hernandobun@gmail.com>
2024-03-07 15:02:47 +0700

    535230133 Hernando Bun

---
+++ soal_1.js
@@ -0,0 +1,16 @@
+function BilPrima(num){
+    for(let i = 2; i<= Math.sqrt(num); i++){
+        if (num % i === 0){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+for(let i = 1; i < 1000; i++){
+    if(BilPrima(i)){
+        console.log(i);
+    }
+}
